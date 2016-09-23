c7c9568285e4bf59c8f1f8a6f29500e1f2301e9b
Dirk Thomas <dthomas@osrfoundation.org>
2016-09-23 16:06:41 -0700

    add script to export pdf using decktape

---
+++ pdf.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+docker run --rm -v `pwd`:/slides astefanutti/decktape http://www.dirk-thomas.net/slides_roscon2016_ros-buildfarm/2016-10-09_roscon_ros-buildfarm.html 2016-10-09_roscon_ros-buildfarm.pdf
